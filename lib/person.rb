19fc5eab5708c82e36328aed02db1ef1f2ac118d
Douglas Uretsky <douglasuretsky@gmail.com>
2018-09-28 19:29:24 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+  attr_accessor :name, :job
+  
+  def initialize
+    @name
+    @job
+  end
+  
+  # def name=(new_name)
+  #   @name = new_name
+  # end
+  
+  # def name
+  #   @name
+  # end
+  
+  # def job=(new_job)
+  #   @job = new_job
+  # end
+  
+  # def job
+  #   @job
+  # end
+end
