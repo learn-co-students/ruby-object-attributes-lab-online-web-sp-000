62328b755c8680ec8edf4e29e354718bb5624142
Sharon A <sharon.alajajian@gmail.com>
2018-10-01 11:23:08 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+    
+    ##setter methods make the name writable
+    def name= (dogs_name)
+        @name = dogs_name
+    end
+    
+    ##getter or reader method
+    def name
+        @name
+    end
+    
+    def breed= (mybreed)
+        @breed = mybreed
+    end
+    
+    def breed
+        @breed
+    end
+    
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    #writes the name of the person to an instance variable @name
+    def name= (myname)
+        @name = myname
+    end
+    
+    #reads the name of the person from an instance variable @name
+    def name
+        @name
+    end
+    
+    def job= (myjob)
+        @job = myjob
+    end
+    
+    def job
+        @job
+    end
+end
