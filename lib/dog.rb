53d5ea2faf2f64759e6c2487ab785ee20cfbfc64
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-17 21:44:49 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+#set name
+  def name=(name)
+    @name = name
+  end
+
+#get name
+  def name
+    @name
+  end
+
+#set breed
+  def breed=(breed)
+    @breed = breed
+  end
+
+# get breed
+  def breed
+    @breed
+  end
+end
