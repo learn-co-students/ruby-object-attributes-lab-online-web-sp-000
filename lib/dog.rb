aa3e5e52a10c983e512df5258cdd5f2689088966
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-20 22:52:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  def name=(inputted_name)
+    @name = inputted_name
+  end 
+  
+  def name 
+    @name
+  end
+  
+  def breed=(inputted_breed)
+    @breed = inputted_breed
+  end
+  
+  def breed
+    @breed 
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+def name=(inputted_name)
+  @name = inputted_name
+end 
+
+def name 
+  @name 
+end
+
+def job=(inputted_job)
+  @job = inputted_job
+end
+
+def job
+  @job
+end
+
+end
