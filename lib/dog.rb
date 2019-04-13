989c45fc505714a4ff057eecb8163c290baf3c32
Andrew Silva <andrewsilva9@gmail.com>
2019-04-13 17:43:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,22 @@
 class Dog
-  def initialize(mookie)
-    @name = mookie
+  def name=(dogs_name)
+    @name = dogs_name
   end
   def name
     @name
   end
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+  def breed
+    @breed
+  end
 end
+
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name
+
+beagle = Dog.new
+beagle.breed = "Beagle"
+puts beagle.breed
