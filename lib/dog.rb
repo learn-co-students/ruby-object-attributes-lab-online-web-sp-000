cbb91168d2920a47fd37d4a4e143c967b8c92275
Ramon Chavez <rachavez1974@gmail.com>
2019-05-01 22:14:11 -0400

    Finish ruby-object-attributes-lab-online-web-sp-000

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  #assigns a name to a dog
+  def name=(name)
+    @name = name
+  end
+
+  #returns a name of a dog
+  def name
+    @name
+  end
+
+  #assigns a breed to a dog
+  def breed=(breed)
+    @breed = breed
+  end
+
+  #returns the breed of a dog
+  def breed
+    @breed
+  end
+
+
+end
