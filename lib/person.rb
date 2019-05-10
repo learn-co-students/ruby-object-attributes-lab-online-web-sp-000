539a5050141455bbedea41e76b6f1ec4c46ce871
Bernadette Davis <bsd231@nyu.edu>
2019-05-10 16:23:01 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(dogName)
+    @name = dogName
+  end
+
+  def name
+    @name
+  end
+
+  def job=(occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+  
+end
