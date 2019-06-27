7736aab5c5680f6bea8839914f26875ff205c9d7
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-27 15:28:43 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(this_person)
+    @name = this_person
+  end
+  def name
+    @name
+  end
+  def job=(their_job)
+    @job = their_job
+  end
+  def job
+    @job
+  end
+end
