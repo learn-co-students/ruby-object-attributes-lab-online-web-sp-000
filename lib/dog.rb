7703519d526362f94d498fbca3244fc6dcc092c3
Alice Brunel <alc.brunel@gmail.com>
2019-07-14 14:10:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  
+  def name=(doggy)
+    @name = doggy
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
