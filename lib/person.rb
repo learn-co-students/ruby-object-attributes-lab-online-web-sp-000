6def92f0f196bb4c8ff50c7682ddc17447a20240
Erika Davis <davis2erika@gmail.com>
2019-07-23 01:41:06 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+end
+
+class Person
+    def job
+    @job
+  end
+
+  def job=(person_job)
+    @job = person_job
+  end
+end
