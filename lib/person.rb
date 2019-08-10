357834832ee63b1deee692ef5e557399f13cd430
Christopher Dent <christopherdent01@gmail.com>
2019-08-10 03:47:12 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person 
+  
+  
+def name
+  @name
+end 
+
+def name=(person_name)
+  @name = person_name
+end 
+
+
+def job
+  @job 
+end 
+
+def job=(his_job)
+  @job = his_job
+end 
+
+end 
