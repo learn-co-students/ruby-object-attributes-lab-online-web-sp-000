7c382a84f2ce5c985562498ef1a7cab5548c6667
Michael Lynch <michael@michaelrlynch.com>
2019-08-27 00:22:52 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_type)
+    @breed = dog_type
+  end
+  
+  def breed
+    @breed
+  end
+
+end
+
