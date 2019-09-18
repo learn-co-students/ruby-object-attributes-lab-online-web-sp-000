93b4135e6ab274ec77216c859a84ebfc980a574a
Andrew Williams <1williamsaj@gmail.com>
2019-09-18 01:12:32 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+
+  def initiliaze(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(alt_name)
+    @name = alt_name
+  end
+
+  def initiliaze(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  def job=(alt_job)
+    @job = alt_job
+  end
+
+end
