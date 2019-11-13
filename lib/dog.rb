60b103cb410ee721409092b986c22f0cfb4c02d6
Riley Slayden <slaydenriley@gmail.com>
2019-11-13 16:53:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,35 @@
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
+
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+end
