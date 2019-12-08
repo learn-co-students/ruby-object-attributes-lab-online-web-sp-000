08da13ec4df16a69f69c96f71177ea012ad847f6
Erin  Rose <errose10@gmail.com>
2019-12-08 16:44:07 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_title)
+    @job = job_title
+  end
+
+  def job
+    @job
+  end
+end
