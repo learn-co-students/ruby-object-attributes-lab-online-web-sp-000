9070871679cba95ac13eb3891c9d157a1946aa69
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-28 04:08:44 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def job=(occupation)
+    @job = occupation
+  end
+
+  def job
+    @job
+  end
+  def name=(moniker)
+    @name = moniker
+  end
+
+  def name
+    @name
+  end
+end
