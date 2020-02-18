7ebab78f5099ca882268893767bb71c616745623
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-18 15:54:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+
+class Dog
+  def name=(doggy)
+    @name = doggy
+  end
+  def name
+    @name
+  end
+  def breed=(liniage)
+    @breed = liniage
+  end
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+
+class Person
+  def name=(me)
+    @name=me
+  end
+  def name
+    @name
+  end
+  def job=(work)
+    @job = work
+  end
+  def job
+    @job
+
+  end
+end
