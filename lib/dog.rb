6f1e757d0407799b5108262071317fe59b611979
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-03-19 18:41:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def name=(dog_name)
+    @name=dog_name
+  end
+    def name 
+      name=@name 
+    end 
+    def breed=(breed_name)
+      @breed=breed_name 
+    end 
+    def breed
+      breed=@breed
+    end 
+end 
