89daab83aedab031ceacb7f8550ed164ff946202
Harrison Price <hprice518@gmail.com>
2021-04-06 22:41:58 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,12 @@ class Dog
   def initialize(name)
     @name = name
   end
-    
-  def name= (name)
+  
+  def name 
     @name = name
   end
   
-  def name 
+  def name= (name)
     @name = name
   end
 
