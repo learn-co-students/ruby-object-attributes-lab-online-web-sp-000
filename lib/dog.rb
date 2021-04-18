c274f1446d38f4cc576b937d22b38d3a00dc6ff5
danpaslawsky <danpaslawsky@gmail.com>
2021-04-17 19:26:45 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+
+    def name= (dogs_name)
+        @name = dogs_name
+    end
+
+    def name
+        "#{@name}"
+    end
+
+    def breed= (breed)
+        @dog = breed
+    end
+
+    def breed
+        "#{@dog}"
+    end
+
+end
+
+
+
+#def initialize(name)
+       # @name = name
+    #end
