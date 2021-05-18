7af445b0e41107dab6234e8b3b98057967a88b03
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-18 17:43:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(occupation)
+    @job = occupation
+  end
+end
